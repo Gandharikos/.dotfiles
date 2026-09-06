9c641ab8ccc3783a4d685dd81cca88d4ddffad49
Johnson Hu <johnson.wq.hu@gmail.com>
2026-09-07 00:32:03 +0200

    fix(tailcat): skip flaky upstream tests

--- users/johnson/home/cli/tools/tailcat.nix
+++ users/johnson/home/cli/tools/tailcat.nix
@@ -9,7 +9,10 @@ let
   inherit (lib.modules) mkIf;
   inherit (lib.options) mkEnableOption;
   cfg = config.my.tailcat;
-  tailcat = inputs.tailcat.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  tailcat = inputs.tailcat.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (_old: {
+    # TODO: Remove this override after updating tailcat to a revision with stable E2E tests.
+    doCheck = false;
+  });
 in
 {
   options.my.tailcat.enable = mkEnableOption "Tailcat";
